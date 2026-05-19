4933d2417ad60b25cee0ed8857af45a6fdfedc46
Soares <mattheussbarbosa@hotmail.com>
2026-05-18 22:24:08 -0300

    adding AppSizes

---
+++ lib/shared_ui/utils/app_sizes.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+/// Constant sizes to be used in the app (paddings, gaps, rounded corners etc.)
+class Sizes {
+  static const p4 = 4.0;
+  static const p8 = 8.0;
+  static const p12 = 12.0;
+  static const p16 = 16.0;
+  static const p20 = 20.0;
+  static const p24 = 24.0;
+  static const p32 = 32.0;
+  static const p40 = 40.0;
+  static const p48 = 48.0;
+  static const p64 = 64.0;
+  static const p80 = 80.0;
+  static const p100 = 100.0;
+  static const p120 = 120.0;
+}
+
+/// Constant gap widths
+const gapW4 = SizedBox(width: Sizes.p4);
+const gapW8 = SizedBox(width: Sizes.p8);
+const gapW12 = SizedBox(width: Sizes.p12);
+const gapW16 = SizedBox(width: Sizes.p16);
+const gapW20 = SizedBox(width: Sizes.p20);
+const gapW24 = SizedBox(width: Sizes.p24);
+const gapW32 = SizedBox(width: Sizes.p32);
+const gapW48 = SizedBox(width: Sizes.p48);
+const gapW64 = SizedBox(width: Sizes.p64);
+const gapW80 = SizedBox(width: Sizes.p80);
+const gapW100 = SizedBox(width: Sizes.p100);
+const gapW120 = SizedBox(width: Sizes.p120);
+
+/// Constant gap heights
+const gapH4 = SizedBox(height: Sizes.p4);
+const gapH8 = SizedBox(height: Sizes.p8);
+const gapH12 = SizedBox(height: Sizes.p12);
+const gapH16 = SizedBox(height: Sizes.p16);
+const gapH20 = SizedBox(height: Sizes.p20);
+const gapH24 = SizedBox(height: Sizes.p24);
+const gapH32 = SizedBox(height: Sizes.p32);
+const gapH48 = SizedBox(height: Sizes.p48);
+const gapH64 = SizedBox(height: Sizes.p64);
+const gapH80 = SizedBox(height: Sizes.p80);
+const gapH100 = SizedBox(height: Sizes.p100);
+const gapH120 = SizedBox(height: Sizes.p120);
+
+/// Constant gap widths
+const gapSliverW4 = SliverToBoxAdapter(child: gapW4);
+const gapSliverW8 = SliverToBoxAdapter(child: gapW8);
+const gapSliverW12 = SliverToBoxAdapter(child: gapW12);
+const gapSliverW16 = SliverToBoxAdapter(child: gapW16);
+const gapSliverW20 = SliverToBoxAdapter(child: gapW20);
+const gapSliverW24 = SliverToBoxAdapter(child: gapW24);
+const gapSliverW32 = SliverToBoxAdapter(child: gapW32);
+const gapSliverW48 = SliverToBoxAdapter(child: gapW48);
+const gapSliverW64 = SliverToBoxAdapter(child: gapW64);
+const gapSliverW80 = SliverToBoxAdapter(child: gapW80);
+const gapSliverW100 = SliverToBoxAdapter(child: gapW100);
+const gapSliverW120 = SliverToBoxAdapter(child: gapW120);
+
+/// Constant gapSliver heights
+const gapSliverH4 = SliverToBoxAdapter(child: gapH4);
+const gapSliverH8 = SliverToBoxAdapter(child: gapH8);
+const gapSliverH12 = SliverToBoxAdapter(child: gapH12);
+const gapSliverH16 = SliverToBoxAdapter(child: gapH16);
+const gapSliverH20 = SliverToBoxAdapter(child: gapH20);
+const gapSliverH24 = SliverToBoxAdapter(child: gapH24);
+const gapSliverH32 = SliverToBoxAdapter(child: gapH32);
+const gapSliverH48 = SliverToBoxAdapter(child: gapH48);
+const gapSliverH64 = SliverToBoxAdapter(child: gapH64);
+const gapSliverH80 = SliverToBoxAdapter(child: gapH80);
+const gapSliverH100 = SliverToBoxAdapter(child: gapH100);
+const gapSliverH120 = SliverToBoxAdapter(child: gapH120);
