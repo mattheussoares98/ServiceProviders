825ea5bdd6b8f558f6e8c90d1cf9cfdca66b1444
Soares <mattheussbarbosa@hotmail.com>
2026-06-07 09:48:48 -0300

    test: Add mock classes for Maintenance Plans data sources

--- testing/mocks/data_source_mocks.dart
+++ testing/mocks/data_source_mocks.dart
@@ -13,6 +13,8 @@ import 'package:clean_architecture/features/work_orders/data/data_sources/work_o
 import 'package:clean_architecture/features/work_orders/data/data_sources/work_orders_remote_data_source.dart';
 import 'package:clean_architecture/features/checklists/data/data_sources/checklists_local_data_source.dart';
 import 'package:clean_architecture/features/checklists/data/data_sources/checklists_remote_data_source.dart';
+import 'package:clean_architecture/features/maintenance_plans/data/data_sources/maintenance_plans_local_data_source.dart';
+import 'package:clean_architecture/features/maintenance_plans/data/data_sources/maintenance_plans_remote_data_source.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockAuthRemoteDataSource extends Mock implements AuthRemoteDataSource {}
@@ -56,3 +58,9 @@ class MockChecklistsRemoteDataSource extends Mock
 
 class MockChecklistsLocalDataSource extends Mock
     implements ChecklistsLocalDataSource {}
+
+class MockMaintenancePlansRemoteDataSource extends Mock
+    implements MaintenancePlansRemoteDataSource {}
+
+class MockMaintenancePlansLocalDataSource extends Mock
+    implements MaintenancePlansLocalDataSource {}
