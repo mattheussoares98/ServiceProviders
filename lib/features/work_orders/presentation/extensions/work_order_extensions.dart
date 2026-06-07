7358b74abda06a52b8d70a9f3d45e98701d7d1f3
Soares <mattheussbarbosa@hotmail.com>
2026-06-07 16:39:59 -0300

    creating extension to get the color

---
+++ lib/features/work_orders/presentation/extensions/work_order_extensions.dart
@@ -0,0 +1,35 @@
+import 'package:clean_architecture/features/work_orders/domain/entities/priority.dart';
+import 'package:clean_architecture/features/work_orders/domain/entities/work_order_status.dart';
+import 'package:flutter/material.dart';
+
+extension PriorityUiExtension on Priority {
+  Color get color {
+    switch (this) {
+      case Priority.low:
+        return Colors.green;
+      case Priority.medium:
+        return Colors.blue;
+      case Priority.high:
+        return Colors.orange;
+      case Priority.critical:
+        return Colors.red;
+    }
+  }
+}
+
+extension WorkOrderStatusUiExtension on WorkOrderStatus {
+  Color get color {
+    switch (this) {
+      case WorkOrderStatus.open:
+        return Colors.grey;
+      case WorkOrderStatus.inProgress:
+        return Colors.amber;
+      case WorkOrderStatus.onHold:
+        return Colors.purple;
+      case WorkOrderStatus.completed:
+        return Colors.green;
+      case WorkOrderStatus.cancelled:
+        return Colors.red;
+    }
+  }
+}
