fc0233037af1a5831276b47dcbee1b05dba68981
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-06-14 18:15:29 -0300

    fixing Hero anamition

--- lib/features/home/presentation/pages/home_page/home_page.dart
+++ lib/features/home/presentation/pages/home_page/home_page.dart
@@ -3,14 +3,17 @@ import 'package:clean_architecture/features/company/presentation/cubits/company/
 import 'package:clean_architecture/features/home/presentation/cubits/home/home_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get_it/get_it.dart';
 
 @RoutePage()
-class HomePage extends StatelessWidget {
+class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = useMemoized(HeroController.new);
+
     return MultiBlocProvider(
       providers: [
         BlocProvider<HomeCubit>(create: (context) => GetIt.I<HomeCubit>()),
@@ -18,7 +21,10 @@ class HomePage extends StatelessWidget {
           create: (context) => GetIt.I<CompanyCubit>()..loadCompany(),
         ),
       ],
-      child: const AutoRouter(),
+      child: HeroControllerScope(
+        controller: controller,
+        child: const AutoRouter(),
+      ),
     );
   }
 }
