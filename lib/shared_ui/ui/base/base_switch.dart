e89103609864bbd120e558f010c3cb6f07045a37
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-06-14 16:29:18 -0300

    little improvements

--- lib/shared_ui/ui/base/base_switch.dart
+++ lib/shared_ui/ui/base/base_switch.dart
@@ -18,34 +18,24 @@ class DefaultSwitch extends StatelessWidget {
   Widget build(BuildContext context) {
     if (title == null) {
       if (context.isCupertino) {
-        return CupertinoSwitch(
-          value: value,
-          onChanged: onChanged,
-        );
+        return CupertinoSwitch(value: value, onChanged: onChanged);
       }
-      return Switch.adaptive(
-        value: value,
-        onChanged: onChanged,
-      );
+      return Switch.adaptive(value: value, onChanged: onChanged);
     }
 
-    final theme = Theme.of(context);
-    final textStyle = theme.textTheme.titleSmall;
+    final textStyle = context.theme.textTheme.titleSmall;
 
     if (context.isCupertino) {
       return CupertinoListTile(
-        title: Text(title!, style: textStyle),
+        title: Text(title!, style: textStyle, maxLines: 3),
         padding: EdgeInsets.zero,
-        trailing: CupertinoSwitch(
-          value: value,
-          onChanged: onChanged,
-        ),
+        trailing: CupertinoSwitch(value: value, onChanged: onChanged),
       );
     } else {
       return SwitchListTile.adaptive(
         dense: true,
         contentPadding: EdgeInsets.zero,
-        title: Text(title!, style: textStyle),
+        title: Text(title!, style: textStyle, maxLines: 3),
         value: value,
         onChanged: onChanged,
       );
