9405a6313398963a6ceef974d780e7d4b1ce345c
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-06-18 21:35:08 -0300

    adding BaseStateView in WorkOrdersPage

--- lib/features/work_orders/presentation/pages/work_orders/work_orders_page.dart
+++ lib/features/work_orders/presentation/pages/work_orders/work_orders_page.dart
@@ -1,10 +1,14 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:clean_architecture/core/utils/extensions/string_extension.dart';
+import 'package:clean_architecture/features/work_orders/domain/entities/work_order_entity.dart';
 import 'package:clean_architecture/features/work_orders/presentation/cubits/work_orders/work_orders_cubit.dart';
 import 'package:clean_architecture/shared_ui/ui/base/app_bar/base_app_bar.dart';
+import 'package:clean_architecture/shared_ui/ui/base/base_list_tile.dart';
 import 'package:clean_architecture/shared_ui/ui/base/base_scaffold.dart';
+import 'package:clean_architecture/shared_ui/ui/base/base_state_view.dart';
 import 'package:clean_architecture/shared_ui/ui/base/buttons/base_icon_button.dart';
 import 'package:clean_architecture/shared_ui/ui/base/platform_icon.dart';
+import 'package:clean_architecture/shared_ui/ui/base/text/base_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -35,7 +39,43 @@ class WorkOrdersPage extends StatelessWidget {
                 ),
               ),
             ),
-            body: const Column(),
+            body:
+                BaseStateView<
+                  WorkOrdersCubit,
+                  WorkOrdersState,
+                  List<WorkOrderEntity>
+                >(
+                  dataSelector: (state) => state.workOrders,
+                  onRetry: context
+                      .read<WorkOrdersCubit>()
+                      .loadWorkOrdersAndChangeRequests,
+                  builder: (context, workOrders) {
+                    if (workOrders.isEmpty) {
+                      return Center(
+                        child: BaseText.titleMedium(
+                          'Nenhum local cadastrado'.hardcoded,
+                          color: Colors.red,
+                        ),
+                      );
+                    }
+
+                    return ListView.builder(
+                      itemCount: workOrders.length,
+                      itemBuilder: (context, index) {
+                        final workOrder = workOrders[index];
+                        return BaseListTile(
+                          title: workOrder.title,
+                          subtitle: workOrder.description,
+                          platformIcon: const PlatformIcon(
+                            materialIcon: Icons.build,
+                            cupertinoIcon: CupertinoIcons.settings,
+                          ),
+                          onTap: () {},
+                        );
+                      },
+                    );
+                  },
+                ),
           );
         },
       ),
