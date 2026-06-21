d24dd36bf789cf1537189612668a16fd49f82c2a
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-06-21 20:21:23 -0300

    disabling the button while is loading, deleting or updating

--- lib/features/locations/presentation/pages/locations/widgets/location_card/add_area_button.dart
+++ lib/features/locations/presentation/pages/locations/widgets/location_card/add_area_button.dart
@@ -2,6 +2,7 @@ import 'package:clean_architecture/core/utils/extensions/string_extension.dart';
 import 'package:clean_architecture/features/locations/domain/entities/location_entity.dart';
 import 'package:clean_architecture/features/locations/presentation/cubits/locations/locations_cubit.dart';
 import 'package:clean_architecture/features/locations/presentation/pages/locations/widgets/create_update_area/create_update_area_dialog.dart';
+import 'package:clean_architecture/shared_ui/cubits/base/base_cubit.dart';
 import 'package:clean_architecture/shared_ui/ui/base/buttons/base_text_button.dart';
 import 'package:clean_architecture/shared_ui/ui/base/platform_icon.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,19 +15,28 @@ class AddAreaButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = context.select<LocationsCubit, bool>(
+      (cubit) =>
+          cubit.state.status == StateStatus.loading ||
+          cubit.state.status == StateStatus.deleting ||
+          cubit.state.status == StateStatus.updating,
+    );
+
     return BaseTextButton(
-      onPressed: () {
-        showDialog<void>(
-          context: context,
-          builder: (_) => BlocProvider.value(
-            value: context.read<LocationsCubit>(),
-            child: CreateUpdateAreaDialog(
-              locationId: location.id,
-              companyId: location.companyId,
-            ),
-          ),
-        );
-      },
+      onPressed: isLoading
+          ? null
+          : () {
+              showDialog<void>(
+                context: context,
+                builder: (_) => BlocProvider.value(
+                  value: context.read<LocationsCubit>(),
+                  child: CreateUpdateAreaDialog(
+                    locationId: location.id,
+                    companyId: location.companyId,
+                  ),
+                ),
+              );
+            },
       platformIcon: const PlatformIcon(
         materialIcon: Icons.add,
         cupertinoIcon: CupertinoIcons.add,
