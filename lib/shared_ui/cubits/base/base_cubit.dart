402f1221605975aa2902f49f345720e3d0b531ea
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-06-21 19:12:44 -0300

    adding new status

--- lib/shared_ui/cubits/base/base_cubit.dart
+++ lib/shared_ui/cubits/base/base_cubit.dart
@@ -8,7 +8,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 /// * [loaded] - The state when data loads without any issue.
 /// * [error] - The state when there is an error while loading the data.
 /// * [noInternet] - The state when there is no internet connection.
-enum StateStatus { initial, loading, loaded, error, noInternet }
+enum StateStatus {
+  initial,
+  loading,
+  loaded,
+  error,
+  noInternet,
+  updating,
+  deleting,
+}
 
 abstract class BaseState extends Equatable {
   const BaseState({this.status = StateStatus.initial, this.errorMessage});
