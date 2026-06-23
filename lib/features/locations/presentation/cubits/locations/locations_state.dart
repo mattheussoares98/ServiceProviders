e6e1b66a3be9cfcdb469d057a746c17455d4ce5e
Soares <mattheussbarbosa@hotmail.com>
2026-06-22 22:48:13 -0300

    adding allAreas in LocationsState

--- lib/features/locations/presentation/cubits/locations/locations_state.dart
+++ lib/features/locations/presentation/cubits/locations/locations_state.dart
@@ -3,6 +3,7 @@ part of 'locations_cubit.dart';
 class LocationsState extends BaseState {
   const LocationsState({
     required this.locations,
+    required this.allAreas,
     required this.areasByLocation,
     this.deletingIds = const {},
     super.status = StateStatus.initial,
@@ -12,15 +13,18 @@ class LocationsState extends BaseState {
   const LocationsState.initial()
     : locations = const [],
       areasByLocation = const <String, List<AreaEntity>>{},
+      allAreas = const [],
       deletingIds = const {},
       super(status: StateStatus.initial, errorMessage: '');
 
   final List<LocationEntity> locations;
   final Map<String, List<AreaEntity>> areasByLocation;
   final Set<String> deletingIds;
+  final List<AreaEntity> allAreas;
 
   LocationsState copyWith({
     List<LocationEntity>? locations,
+    List<AreaEntity>? allAreas,
     Map<String, List<AreaEntity>>? areasByLocation,
     Set<String>? deletingIds,
     StateStatus? status,
@@ -29,6 +33,7 @@ class LocationsState extends BaseState {
   }) {
     return LocationsState(
       locations: locations ?? this.locations,
+      allAreas: allAreas ?? this.allAreas,
       areasByLocation: areasByLocation ?? this.areasByLocation,
       deletingIds: deletingIds ?? this.deletingIds,
       status: status ?? this.status,
@@ -42,6 +47,7 @@ class LocationsState extends BaseState {
   List<Object?> get props => [
     locations,
     areasByLocation,
+    allAreas,
     deletingIds,
     status,
     errorMessage,
