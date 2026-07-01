1b89f9d227453fa6b6f4dd978afe63a8e8d01765
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-07-01 19:07:40 -0300

    adding option to delete and update the work order

--- lib/features/work_orders/presentation/pages/work_orders/widgets/orders_items.dart
+++ lib/features/work_orders/presentation/pages/work_orders/widgets/orders_items.dart
@@ -1,10 +1,16 @@
 import 'package:clean_architecture/core/utils/extensions/string_extension.dart';
 import 'package:clean_architecture/features/work_orders/domain/entities/work_order_entity.dart';
 import 'package:clean_architecture/features/work_orders/presentation/cubits/work_orders/work_orders_cubit.dart';
-import 'package:clean_architecture/shared_ui/ui/base/base_list_tile.dart';
+import 'package:clean_architecture/features/work_orders/presentation/pages/work_orders/widgets/create_update_work_order_form.dart';
+import 'package:clean_architecture/shared_ui/cubits/base/base_cubit.dart';
+import 'package:clean_architecture/shared_ui/ui/base/alert_dialogs.dart';
 import 'package:clean_architecture/shared_ui/ui/base/base_state_view.dart';
+import 'package:clean_architecture/shared_ui/ui/base/buttons/base_icon_button.dart';
+import 'package:clean_architecture/shared_ui/ui/base/buttons/base_text_button.dart';
 import 'package:clean_architecture/shared_ui/ui/base/platform_icon.dart';
+import 'package:clean_architecture/shared_ui/ui/base/show_modal_page.dart';
 import 'package:clean_architecture/shared_ui/ui/base/text/base_text.dart';
+import 'package:clean_architecture/shared_ui/utils/app_sizes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -25,19 +31,79 @@ class OrdersItems extends StatelessWidget {
         if (workOrders.isEmpty) {
           return BaseText.error('Nenhuma ordem foi encontrada'.hardcoded);
         }
+        final isDeleting = context.select((WorkOrdersCubit cubit) {
+          //TODO add the deletingIds to treat it by each one
+          return cubit.state.status == StateStatus.deleting;
+        });
 
         return ListView.builder(
           itemCount: workOrders.length,
           itemBuilder: (context, index) {
             final workOrder = workOrders[index];
-            return BaseListTile(
-              title: workOrder.title,
-              subtitle: workOrder.description,
-              platformIcon: const PlatformIcon(
-                materialIcon: Icons.build,
-                cupertinoIcon: CupertinoIcons.settings,
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(Sizes.p8),
+                child: Column(
+                  crossAxisAlignment: .start,
+                  children: [
+                    BaseText.title(workOrder.title),
+                    gapH4,
+                    if (workOrder.description != null)
+                      BaseText(workOrder.description!),
+                    Row(
+                      mainAxisAlignment: .spaceBetween,
+                      children: [
+                        FittedBox(
+                          child: BaseIconButton(
+                            isLoading: isDeleting,
+                            onPressed: () {
+                              showAlertDialog(
+                                context: context,
+                                title: 'Atenção!'.hardcoded,
+                                contentText:
+                                    'Deseja realmente excluir a ordem de serviço?'
+                                        .hardcoded,
+                                defaultActionText: 'Sim'.hardcoded,
+                                cancelActionText: 'Não'.hardcoded,
+                                onOkPressed: () => context
+                                    .read<WorkOrdersCubit>()
+                                    .deleteWorkOrder(workOrder.id),
+                              );
+                            },
+                            platformIcon: const PlatformIcon(
+                              materialIcon: Icons.delete,
+                              cupertinoIcon: CupertinoIcons.delete,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: BaseTextButton(
+                            text: 'Editar'.hardcoded,
+                            onPressed: isDeleting
+                                ? null
+                                : () {
+                                    showModalPage<void>(
+                                      BlocProvider.value(
+                                        value: context.read<WorkOrdersCubit>(),
+                                        child: CreateUpdateWorkOrderForm(
+                                          workOrder: workOrder,
+                                        ),
+                                      ),
+                                      context,
+                                    );
+                                  },
+                            platformIcon: const PlatformIcon(
+                              materialIcon: Icons.edit,
+                              cupertinoIcon: CupertinoIcons.pencil,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              onTap: () {},
             );
           },
         );
