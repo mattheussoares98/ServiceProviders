a91309ac56aaa096af4e922171928661620b1b23
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-07-02 21:05:09 -0300

    fixing problem for running the app in some browsers and removing deprecated import

--- lib/core/clients/local/drift/connection/web.dart
+++ lib/core/clients/local/drift/connection/web.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
 import 'package:drift/drift.dart';
 import 'package:drift/wasm.dart';
-import 'package:drift/web.dart';
+import 'package:sqlite3/wasm.dart';
 
 QueryExecutor connect() {
   return LazyDatabase(() async {
@@ -9,10 +10,19 @@ QueryExecutor connect() {
         databaseName: 'app',
         sqlite3Uri: Uri.parse('sqlite3.wasm'),
         driftWorkerUri: Uri.parse('drift_worker.js'),
+      ).timeout(
+        const Duration(seconds: 5),
+        onTimeout: () => throw TimeoutException('WasmDatabase initialization timed out'),
       );
       return result.resolvedExecutor;
     } catch (_) {
-      return WebDatabase.withStorage(DriftWebStorage.volatile());
+      try {
+        final sqlite3 = await WasmSqlite3.loadFromUrlString('sqlite3.wasm');
+        sqlite3.registerVirtualFileSystem(InMemoryFileSystem(), makeDefault: true);
+        return WasmDatabase.inMemory(sqlite3);
+      } catch (e) {
+        rethrow;
+      }
     }
   });
 }
