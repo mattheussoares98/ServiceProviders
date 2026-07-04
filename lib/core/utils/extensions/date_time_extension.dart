df6108422bc45bb5bf59b7304938961bd31b529f
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-07-04 17:36:04 -0300

    adding more patterns

--- lib/core/utils/extensions/date_time_extension.dart
+++ lib/core/utils/extensions/date_time_extension.dart
@@ -4,8 +4,22 @@ import 'package:intl/intl.dart';
 
 extension DateTimeExtension on DateTime {
   String formatDate([DateFormatType type = DateFormatType.yMMMd]) {
-    return DateFormat(type.name).format(this);
+    return DateFormat(type.pattern).format(this);
   }
 }
 
-enum DateFormatType { yMd, yMMMMd, yMMMd, yMMMEd, yMMMMEEEEd, yMdHm, Hm, Hms }
+enum DateFormatType {
+  yMd('yMd'),
+  yMMMMd('yMMMMd'),
+  yMMMd('yMMMd'),
+  yMMMEd('yMMMEd'),
+  yMMMMEEEEd('yMMMMEEEEd'),
+  yMdHm('yMdHm'),
+  Hm('Hm'),
+  Hms('Hms'),
+  ddMMyyyy('dd/MM/yyyy'),
+  ddMMyyyyHHmm('dd/MM/yyyy HH:mm');
+
+  const DateFormatType(this.pattern);
+  final String pattern;
+}
