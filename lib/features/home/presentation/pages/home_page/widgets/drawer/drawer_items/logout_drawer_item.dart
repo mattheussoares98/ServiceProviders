b4760288ea2fe590126216d0f1fa76a0744b4f89
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-07-04 20:56:47 -0300

    asking if really want to exit

--- lib/features/home/presentation/pages/home_page/widgets/drawer/drawer_items/logout_drawer_item.dart
+++ lib/features/home/presentation/pages/home_page/widgets/drawer/drawer_items/logout_drawer_item.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:clean_architecture/core/utils/extensions/string_extension.dart';
 import 'package:clean_architecture/features/home/presentation/cubits/home/home_cubit.dart';
+import 'package:clean_architecture/shared_ui/ui/base/alert_dialogs.dart';
 import 'package:clean_architecture/shared_ui/ui/base/base_drawer_item.dart';
 import 'package:clean_architecture/shared_ui/ui/base/platform_icon.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,13 +16,23 @@ class LogoutDrawerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseDrawerItem(
       title: 'Sair'.hardcoded,
+      closeAutomatically: false,
       platformIcon: const PlatformIcon(
         materialIcon: Icons.logout,
         cupertinoIcon: CupertinoIcons.square_arrow_right,
         color: Colors.red,
       ),
       onTap: () async {
-        await context.read<HomeCubit>().logout();
+        await showAlertDialog(
+          context: context,
+          title: 'Deseja realmente sair?'.hardcoded,
+          onOkPressed: () {
+            context.pop();
+            context.read<HomeCubit>().logout();
+          },
+          cancelActionText: 'Não'.hardcoded,
+          defaultActionText: 'Sim'.hardcoded,
+        );
       },
     );
   }
