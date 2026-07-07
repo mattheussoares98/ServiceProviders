82ae83f0b282e699c8216e726fcf139ed36896f9
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-07-06 21:34:14 -0300

    implementing AttachmentsRemoteDataSource

--- lib/features/attachments/data/data_sources/attachments_remote_data_source.dart
+++ lib/features/attachments/data/data_sources/attachments_remote_data_source.dart
@@ -1,7 +1,28 @@
 import 'package:injectable/injectable.dart';
 import 'package:o_jogo_da_obra/core/clients/remote/http/http_client.dart';
+import 'package:o_jogo_da_obra/core/clients/remote/storage/storage_client.dart';
+import 'package:o_jogo_da_obra/core/constants/api_endpoints.dart';
+import 'package:o_jogo_da_obra/core/data/handlers/api_handler.dart';
+import 'package:o_jogo_da_obra/core/utils/type_defs.dart';
+import 'package:o_jogo_da_obra/features/attachments/data/models/responses/attachment_response_model.dart';
 
-abstract interface class AttachmentsRemoteDataSource {}
+abstract interface class AttachmentsRemoteDataSource {
+  /// Requests a time-limited presigned PUT URL from the backend Edge Function.
+  ///
+  /// [objectKey] must follow the convention built by [StorageClient.buildObjectKey].
+  FutureData<PresignedUrlResponse> getPresignedUploadUrl(String objectKey);
+
+  /// Saves [remoteUrl] on the attachment record after a successful R2 upload.
+  FutureBool confirmUpload({
+    required String attachmentId,
+    required String remoteUrl,
+  });
+
+  /// Fetches all non-deleted attachments for a given work order from the API.
+  FutureList<AttachmentResponseModel> getAttachmentsByWorkOrder(
+    String workOrderId,
+  );
+}
 
 @LazySingleton(as: AttachmentsRemoteDataSource)
 final class AttachmentsRemoteDataSourceImpl
@@ -10,4 +31,40 @@ final class AttachmentsRemoteDataSourceImpl
     : _httpClient = httpClient;
 
   final HttpClient _httpClient;
+
+  @override
+  FutureData<PresignedUrlResponse> getPresignedUploadUrl(String objectKey) =>
+      ApiHandler.call(
+        () => _httpClient.post<MapDynamic>(
+          ApiEndpoints.presignedUploadUrl,
+          data: {'object_key': objectKey},
+        ),
+        fromJson: (json) => PresignedUrlResponse(
+          uploadUrl: json['upload_url'] as String,
+          fileKey: json['file_key'] as String,
+        ),
+      );
+
+  @override
+  FutureBool confirmUpload({
+    required String attachmentId,
+    required String remoteUrl,
+  }) => ApiHandler.staticCall(
+    () => _httpClient.patch<void>(
+      '${ApiEndpoints.attachments}/$attachmentId/confirm',
+      data: {'remote_url': remoteUrl},
+    ),
+    staticData: true,
+  );
+
+  @override
+  FutureList<AttachmentResponseModel> getAttachmentsByWorkOrder(
+    String workOrderId,
+  ) => ApiHandler.call<List<AttachmentResponseModel>, AttachmentResponseModel>(
+    () => _httpClient.get<MapDynamic>(
+      ApiEndpoints.attachments,
+      queryParameters: {'work_order_id': workOrderId},
+    ),
+    fromJson: AttachmentResponseModel.fromJson,
+  );
 }
---
+++ test/features/attachments/data/data_sources/attachments_remote_data_source_test.dart
@@ -0,0 +1,198 @@
+import 'package:dio/dio.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:o_jogo_da_obra/core/clients/remote/storage/storage_client.dart';
+import 'package:o_jogo_da_obra/core/constants/api_endpoints.dart';
+import 'package:o_jogo_da_obra/core/data/states/data_state.dart';
+import 'package:o_jogo_da_obra/core/utils/type_defs.dart';
+import 'package:o_jogo_da_obra/features/attachments/data/data_sources/attachments_remote_data_source.dart';
+import 'package:o_jogo_da_obra/features/attachments/data/models/responses/attachment_response_model.dart';
+
+import '../../../../../testing/mocks/client_mocks.dart';
+import '../../../../../testing/mocks/entity_factory.dart';
+
+Response<T> _mockResponse<T>(T data, {int statusCode = 200}) => Response(
+  data: data,
+  statusCode: statusCode,
+  requestOptions: RequestOptions(),
+);
+
+void main() {
+  final faker = Faker();
+  late MockHttpClient mockHttpClient;
+  late AttachmentsRemoteDataSourceImpl dataSource;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    dataSource = AttachmentsRemoteDataSourceImpl(httpClient: mockHttpClient);
+  });
+
+  group('getPresignedUploadUrl', () {
+    final tObjectKey = StorageClient.buildObjectKey(
+      companyId: faker.guid.guid(),
+      workOrderId: faker.guid.guid(),
+      uuid: faker.guid.guid(),
+      extension: 'webp',
+    );
+    final tUploadUrl = 'https://bucket.r2.dev/$tObjectKey?sig=abc';
+
+    test(
+      'should return SuccessState<PresignedUrlResponse> when API returns correct data',
+      () async {
+        // Arrange
+        when(
+          () =>
+              mockHttpClient.post<MapDynamic>(any(), data: any(named: 'data')),
+        ).thenAnswer(
+          (_) async => _mockResponse<MapDynamic>({
+            'data': {
+              'upload_url': tUploadUrl,
+              'file_key': tObjectKey,
+            }
+          }),
+        );
+
+        // Act
+        final result = await dataSource.getPresignedUploadUrl(tObjectKey);
+
+        // Assert
+        expect(result, isA<SuccessState<PresignedUrlResponse>>());
+        final data = (result as SuccessState<PresignedUrlResponse>).data;
+        expect(data!.uploadUrl, tUploadUrl);
+        expect(data.fileKey, tObjectKey);
+      },
+    );
+
+    test('should return FailureState when API call fails', () async {
+      // Arrange
+      when(
+        () => mockHttpClient.post<MapDynamic>(
+          ApiEndpoints.presignedUploadUrl,
+          data: any(named: 'data'),
+        ),
+      ).thenThrow(
+        DioException(
+          requestOptions: RequestOptions(),
+          type: DioExceptionType.badResponse,
+          response: Response(
+            statusCode: 400,
+            requestOptions: RequestOptions(),
+            data: {'message': 'Error requesting URL'},
+          ),
+        ),
+      );
+
+      // Act
+      final result = await dataSource.getPresignedUploadUrl(tObjectKey);
+
+      // Assert
+      expect(result, isA<FailureState<PresignedUrlResponse>>());
+    });
+  });
+
+  group('confirmUpload', () {
+    final tAttachmentId = faker.guid.guid();
+    final tRemoteUrl = faker.internet.httpsUrl();
+
+    test(
+      'should return SuccessState<bool>(true) when API update is successful',
+      () async {
+        // Arrange
+        when(
+          () => mockHttpClient.patch<void>(
+            '${ApiEndpoints.attachments}/$tAttachmentId/confirm',
+            data: {'remote_url': tRemoteUrl},
+          ),
+        ).thenAnswer((_) async => _mockResponse<void>(null));
+
+        // Act
+        final result = await dataSource.confirmUpload(
+          attachmentId: tAttachmentId,
+          remoteUrl: tRemoteUrl,
+        );
+
+        // Assert
+        expect(result, isA<SuccessState<bool>>());
+        expect((result as SuccessState<bool>).data, isTrue);
+      },
+    );
+
+    test('should return FailureState when API update fails', () async {
+      // Arrange
+      when(
+        () => mockHttpClient.patch<void>(any(), data: any(named: 'data')),
+      ).thenThrow(
+        DioException(
+          requestOptions: RequestOptions(),
+          type: DioExceptionType.badResponse,
+        ),
+      );
+
+      // Act
+      final result = await dataSource.confirmUpload(
+        attachmentId: tAttachmentId,
+        remoteUrl: tRemoteUrl,
+      );
+
+      // Assert
+      expect(result, isA<FailureState<bool>>());
+    });
+  });
+
+  group('getAttachmentsByWorkOrder', () {
+    final tWorkOrderId = faker.guid.guid();
+    final tAttachmentEntity = EntityFactory.makeAttachmentEntity();
+    final tModel = AttachmentResponseModel.fromEntity(tAttachmentEntity);
+
+    test(
+      'should return SuccessState with list of models when API is successful',
+      () async {
+        // Arrange
+        when(
+          () => mockHttpClient.get<MapDynamic>(
+            ApiEndpoints.attachments,
+            queryParameters: {'work_order_id': tWorkOrderId},
+          ),
+        ).thenAnswer(
+          (_) async => _mockResponse<MapDynamic>({
+            'data': [tModel.toJson()]
+          }),
+        );
+
+        // Act
+        final result = await dataSource.getAttachmentsByWorkOrder(tWorkOrderId);
+
+        // Assert
+        expect(result, isA<SuccessState<List<AttachmentResponseModel>>>());
+        final list =
+            (result as SuccessState<List<AttachmentResponseModel>>).data;
+        expect(list, hasLength(1));
+        expect(list![0].id, tModel.id);
+        expect(list[0].fileName, tModel.fileName);
+      },
+    );
+
+    test('should return FailureState when API call fails', () async {
+      // Arrange
+      when(
+        () => mockHttpClient.get<MapDynamic>(
+          any(),
+          queryParameters: any(named: 'queryParameters'),
+        ),
+      ).thenThrow(
+        DioException(
+          requestOptions: RequestOptions(),
+          type: DioExceptionType.badResponse,
+        ),
+      );
+
+      // Act
+      final result = await dataSource.getAttachmentsByWorkOrder(tWorkOrderId);
+
+      // Assert
+      expect(result, isA<FailureState<List<AttachmentResponseModel>>>());
+    });
+  });
+}
+
