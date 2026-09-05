02bdf10b0e45c3a17dae83841ea1329d1cedc0e2
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-09-05 19:04:04 -0300

    feat(users): adding last accessed information

--- lib/features/users/presentation/pages/permissions/users_and_permissions_page/widgets/users.dart
+++ lib/features/users/presentation/pages/permissions/users_and_permissions_page/widgets/users.dart
@@ -2,10 +2,11 @@ import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:o_jogo_da_obra/core/utils/extensions/string_extension.dart';
+import 'package:o_jogo_da_obra/core/utils/extensions/date_time_extension.dart';
 import 'package:o_jogo_da_obra/features/users/domain/entities/user_profile_entity.dart';
 import 'package:o_jogo_da_obra/features/users/presentation/cubits/users/users_cubit.dart';
 import 'package:o_jogo_da_obra/features/users/presentation/pages/permissions/users_and_permissions_page/widgets/permission_item.dart';
+import 'package:o_jogo_da_obra/shared_ui/cubits/session/session_cubit.dart';
 import 'package:o_jogo_da_obra/shared_ui/ui/base/base_image_widget.dart';
 import 'package:o_jogo_da_obra/shared_ui/ui/base/base_state_view.dart';
 import 'package:o_jogo_da_obra/shared_ui/ui/base/platform_icon.dart';
@@ -17,6 +18,11 @@ class Users extends StatelessWidget {
   const Users({super.key});
   @override
   Widget build(BuildContext context) {
+    final sessionUser = context.select<SessionCubit, UserProfileEntity>(
+      (cubit) => cubit.state.user,
+    );
+    final isCurrentUserAdmin = sessionUser.isAdmin;
+
     return BaseStateView<UsersCubit, UsersState, List<UserProfileEntity>>(
       dataSelector: (state) => state.users,
       onRetry: () => context.read<UsersCubit>().loadAll(),
@@ -42,12 +48,21 @@ class Users extends StatelessWidget {
             final roleOrGroup = user.isAdmin
                 ? 'Administrador'.hardcoded
                 : groupName;
+
+            final lastAccessText = isCurrentUserAdmin
+                ? user.lastAccessAt != null
+                      ? '\nÚltimo acesso: ${user.lastAccessAt!.toLocal().formatDate(DateFormatType.ddMMyyyyHHmm)}'
+                            .hardcoded
+                      : '\nÚltimo acesso: Nunca'.hardcoded
+                : null;
+
             return PermissionItem(
               title: user.name,
               subtitle: BaseRichText(
                 texts: [
                   BaseText.title(user.email),
                   BaseText(' • $roleOrGroup'),
+                  if (lastAccessText != null) BaseText.caption(lastAccessText),
                   if (!user.isActive)
                     BaseText(' Pendente'.hardcoded, color: Colors.orange),
                 ],
