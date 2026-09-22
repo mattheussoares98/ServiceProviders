a102e27906a19f17f5bc8f47fb243e6dba3b73a7
Mattheus Soares <mattheussbarbosa@hotmail.com>
2026-09-22 20:05:49 -0300

    style(supoport): replacing manual count controlling with BaseTextFormField property

--- lib/features/support/presentation/pages/support/widgets/support_description_field.dart
+++ lib/features/support/presentation/pages/support/widgets/support_description_field.dart
@@ -5,7 +5,6 @@ import 'package:o_jogo_da_obra/features/support/presentation/cubits/support/supp
 import 'package:o_jogo_da_obra/shared_ui/ui/base/form_field/base_text_form_field.dart';
 import 'package:o_jogo_da_obra/shared_ui/ui/base/text/base_text.dart';
 import 'package:o_jogo_da_obra/shared_ui/utils/app_sizes.dart';
-import 'package:o_jogo_da_obra/shared_ui/utils/extensions/build_context_extension.dart';
 
 class SupportDescriptionField extends StatelessWidget {
   const SupportDescriptionField({
@@ -19,41 +18,22 @@ class SupportDescriptionField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<SupportCubit, SupportState>(
-      buildWhen: (prev, current) =>
-          prev.userDescription != current.userDescription,
-      builder: (context, state) {
-        final currentLength = state.userDescription.length;
-        const maxLength = SupportState.maxDescriptionLength;
-
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BaseText('Como podemos ajudar?'.hardcoded, fontWeight: .bold),
-            gapH8,
-            BaseTextFormField(
-              controller: controller,
-              focusNode: focusNode,
-              maxLines: 6,
-              maxLength: maxLength,
-              hintText: 'Descreva detalhadamente o problema ou sua dúvida...'
-                  .hardcoded,
-              onChanged: context.read<SupportCubit>().updateDescription,
-            ),
-            gapH4,
-            Align(
-              alignment: Alignment.centerRight,
-              child: BaseText(
-                '$currentLength / $maxLength'.hardcoded,
-                textType: TextType.caption,
-                color: currentLength > maxLength
-                    ? context.theme.colorScheme.error
-                    : context.theme.hintColor,
-              ),
-            ),
-          ],
-        );
-      },
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        BaseText('Como podemos ajudar?'.hardcoded, fontWeight: .bold),
+        gapH8,
+        BaseTextFormField(
+          controller: controller,
+          focusNode: focusNode,
+          maxLines: 6,
+          maxLength: SupportState.maxDescriptionLength,
+          showCounter: true,
+          hintText:
+              'Descreva detalhadamente o problema ou sua dúvida...'.hardcoded,
+          onChanged: context.read<SupportCubit>().updateDescription,
+        ),
+      ],
     );
   }
 }
